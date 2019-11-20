5c6d7f481f5b22cf279383f869070abe4c958fa7
Elvin Nilsson Ribbing <elvin.nilssonribbing@elev.ga.lbs.se>
2019-11-20 10:33:19 +0100

    Movement (not finished)

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private Rigidbody rb;
+
+
+    //variables
+    public float moveSpeed = 5;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Thrust"))
+        {
+            rb.velocity = transform.forward * moveSpeed;
+        }
+
+
+    }
+}
