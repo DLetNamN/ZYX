e79df058beca7479f74301842bc2d1f4416d197b
Andreas Styverbjörne <andreas.styverbjorne@elev.ga.lbs.se>
2019-11-21 09:54:30 +0100

    setup the mainmenu script and added the menu scene to the Build

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    GameObject options;
+    GameObject credits;
+    GameObject mainMenu;
+
+    void Start()
+    {
+        options = GameObject.Find("Options");
+        credits = GameObject.Find("Credits");
+        mainMenu = GameObject.Find("MainMenu");
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void OptionsScreen()
+    {
+        mainMenu.SetActive(false);
+        options.SetActive(true);
+    }
+
+    public void CreditsScreen()
+    {
+        mainMenu.SetActive(false);
+        credits.SetActive(true);
+    }
+
+    public void ExitGame()
+    {
+        ExitGame();
+        print("Exited Game");
+    }
+
+    public void BackButton()
+    {
+        options.SetActive(false);
+        credits.SetActive(false);
+        mainMenu.SetActive(true);
+    }
+
+}
