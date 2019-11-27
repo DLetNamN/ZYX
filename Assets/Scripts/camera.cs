73792ec5e621b4bc42db978b53daf59b841550b7
DLetNamN <wissam.hattab@elev.ga.lbs.se>
2019-11-27 10:16:15 +0100

    Made working camera follow script.

---
+++ Assets/Scripts/camera.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera : MonoBehaviour
+{
+    //
+    private Transform playerLocation;
+    private Transform cameraLocation;
+
+    //Configurable
+    public float lerpTVariable = 1;
+
+    void Start()
+    {
+        playerLocation = GameObject.FindWithTag("Player").transform;
+        cameraLocation = gameObject.GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        cameraLerpToPlayer();    
+    }
+
+    void cameraLerpToPlayer()
+    {
+        var playerPos = new Vector3(playerLocation.position.x + 8, 8, playerLocation.position.z + 2);
+
+        cameraLocation.position = Vector3.Lerp(cameraLocation.position, playerPos, lerpTVariable);
+    }
+}
