c3b93f941483178a3018de7590e86c61768f28cf
Elvin Nilsson Ribbing <elvin.nilssonribbing@elev.ga.lbs.se>
2020-01-08 10:42:44 +0100

    shoot animation

--- Assets/Scripts/Shoot.cs
+++ Assets/Scripts/Shoot.cs
@@ -6,15 +6,23 @@ public class Shoot : MonoBehaviour
 {
     public float Force;
     public GameObject bullet;
+    private Animator anim;
 
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
     void Update()
     {
         if (Input.GetButton("Shoot"))
         {
+            anim.SetBool("shoot", true);
             Force *= Time.deltaTime * 60;
         }
         else
         {
+            anim.SetBool("shoot", false);
             Force = 1;
         }
 
