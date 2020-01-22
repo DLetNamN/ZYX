0dc0749b62c04352317c7ce505443072664bbefc
Dolt.Namn <wissamhattab@gmail.com>
2020-01-22 08:45:05 +0100

    Started HP script

--- Assets/Scripts/HP.cs
+++ Assets/Scripts/HP.cs
@@ -4,5 +4,13 @@ using UnityEngine;
 
 public class HP : MonoBehaviour
 {
-    
+    float hp_p1, hp_p2;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "AAt_Shot")
+        {
+
+        }
+    }
 }
