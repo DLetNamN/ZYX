75a0faba2697c3111fe3b99767f09b757aaa0a57
Dolt.Namn <wissamhattab@gmail.com>
2020-01-22 09:05:45 +0100

    completed HP script first draft.
    testing HP script.

--- Assets/Scripts/HP.cs
+++ Assets/Scripts/HP.cs
@@ -4,13 +4,34 @@ using UnityEngine;
 
 public class HP : MonoBehaviour
 {
-    float hp_p1, hp_p2;
+    //player identification
+    int p_int;
+    string p_t_name;
+
+    //player medical
+    int p_hp;
+    [SerializeField] int p_dmg;
+
+    private void Start()
+    {
+        p_t_name = gameObject.tag;
+        p_int = p_t_name[6];
+
+        if (p_int != 1)
+        {
+            p_int -= 1;
+        } 
+        else
+        {
+            p_int += 1;
+        }
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.name == "AAt_Shot")
+        if (collision.gameObject.name == "AAt_Shot " + p_int)
         {
-
+            p_hp -= p_dmg; 
         }
     }
 }
