505074ac98c11e37706a1877fe4e4baca2e4f133
Andreas Styverbjörne <andreas.styverbjorne@elev.ga.lbs.se>
2020-01-29 10:07:53 +0100

    added some music and fixed charging sound effect

---
+++ Assets/Scripts/SwitchMusic.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SwitchMusic : MonoBehaviour
+{
+    AudioSource thisAudioSource;
+
+    public AudioClip CloneArmySong;
+    public AudioClip DroidArmySong;
+
+    private void Start()
+    {
+        thisAudioSource = GetComponent<AudioSource>();
+        thisAudioSource.clip = CloneArmySong;
+        thisAudioSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SwitchSoundtrack();
+    }
+
+    void SwitchSoundtrack()
+    {
+        if (thisAudioSource.clip == CloneArmySong)
+        {
+            if (!thisAudioSource.isPlaying)
+            {
+                thisAudioSource.clip = DroidArmySong;
+                thisAudioSource.Play();
+            }
+        }
+        else if (thisAudioSource.clip == DroidArmySong)
+        {
+            if (!thisAudioSource.isPlaying)
+            {
+                thisAudioSource.clip = CloneArmySong;
+                thisAudioSource.Play();
+            }
+        }
+    }
+}
